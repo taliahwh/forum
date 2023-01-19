9e6d700bc27e99f1c14b7f1f1c70edb87cdd5450
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-19 16:14:24 -0500

    server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import colors from 'colors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+import { ApolloServer } from 'apollo-server';
+
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers/index.js';
+import connectDB from './db.js';
+
+dotenv.config();
+
+connectDB();
+
+// app.use(bodyParser.urlencoded());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Connect to MongoDB
+const PORT = process.env.PORT;
+server.listen({ port: PORT }).then((res) => {
+  console.log(`Server running on at ${res.url}`.green.bold);
+});
