10a51be6ad60b036ba1d565f37906268f11c1ef3
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-19 16:13:48 -0500

    graphql setup

---
+++ server/graphql/typeDefs.js
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    token: String!
+    createdAt: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+  }
+
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+  }
+`;
+
+export default typeDefs;
