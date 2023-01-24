8334a0efb573a8985b899f8c5ce6cc8c935f7226
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-24 15:29:44 -0500

    getPost, createPost

--- server/graphql/resolvers/posts.resolvers.js
+++ server/graphql/resolvers/posts.resolvers.js
@@ -1,4 +1,6 @@
+import { UserInputError } from 'apollo-server';
 import Post from '../../models/postModel.js';
+import checkAuth from '../../util/check-auth.js';
 
 const postResolvers = {
   Query: {
@@ -9,6 +11,37 @@ const postResolvers = {
       } catch (error) {
         throw new Error(err);
       }
+    },
+
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        }
+        throw new Error('Post not found');
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+
+  Mutation: {
+    // can access the req.body within the context parameter
+    async createPost(_, { body }, context) {
+      const authorizedUser = checkAuth(context);
+      console.log(user);
+      console.log(body);
+
+      const newPost = new Post({
+        body,
+        username: authorizedUser.username,
+        user: authorizedUser.id,
+      });
+
+      await newPost.save();
+
+      return newPost;
     },
   },
 };
