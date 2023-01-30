290343b192a5db63f1e18bdc2f7c846d6bb2ed3b
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-30 15:25:25 -0500

    user authentication middleware

---
+++ server/util/check-auth.js
@@ -0,0 +1,26 @@
+import { AuthenticationError } from 'apollo-server';
+import jwt from 'jsonwebtoken';
+
+const checkAuth = (context) => {
+  // conext = { ... headers }
+  const authHeader = context.req.headers.authorization;
+  // console.log(context);
+  if (authHeader) {
+    const token = authHeader.split('Bearer ')[1];
+
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(user);
+        return user;
+      } catch (error) {
+        console.log('Authentication error');
+        throw new AuthenticationError('Invalid/Expired token');
+      }
+    }
+    throw new Error("Authorization token must be 'Bearer [token]");
+  }
+  throw new Error('Authorization header must be provided');
+};
+
+export default checkAuth;
