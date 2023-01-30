de518da1e3020d81ad61aecb6c8d9d0afdff34dd
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-30 16:26:11 -0500

    create and delete posts

--- server/graphql/resolvers/posts.resolvers.js
+++ server/graphql/resolvers/posts.resolvers.js
@@ -1,4 +1,4 @@
-import { UserInputError } from 'apollo-server';
+import { UserInputError, AuthenticationError } from 'apollo-server';
 import Post from '../../models/postModel.js';
 import checkAuth from '../../util/check-auth.js';
 
@@ -6,7 +6,7 @@ const postResolvers = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (error) {
         throw new Error(err);
@@ -32,15 +32,39 @@ const postResolvers = {
     async createPost(_, { body }, context) {
       const authorizedUser = checkAuth(context);
 
-      const newPost = new Post({
-        body,
-        username: authorizedUser.username,
-        user: authorizedUser.id,
-      });
+      try {
+        const newPost = new Post({
+          body,
+          username: authorizedUser.username,
+          user: authorizedUser.id,
+        });
 
-      await newPost.save();
+        await newPost.save();
 
-      return newPost;
+        return newPost;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+
+    async deletePost(_, { postId }, context) {
+      const authorizedUser = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (!post) throw new Error('Post not found by that id');
+
+        if (String(post.user) !== String(authorizedUser.id)) {
+          throw new AuthenticationError('Not authorized to delete post');
+        }
+
+        await post.delete();
+
+        return 'Post deleted from database';
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 };
