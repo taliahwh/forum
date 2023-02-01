58b7f626f465ef19405463d879c9f57a5eea7ace
Taliah Wharton <taliahwh23@yahoo.com>
2023-02-01 09:56:24 -0500

    added comment resolvers

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 import postResolvers from './posts.resolvers.js';
 import userResolvers from './users.resolvers.js';
+import commentResolvers from './comments.resolvers.js';
 
 const resolvers = {
   Query: {
@@ -8,6 +9,7 @@ const resolvers = {
   Mutation: {
     ...userResolvers.Mutation,
     ...postResolvers.Mutation,
+    ...commentResolvers.Mutation,
   },
 };
 
